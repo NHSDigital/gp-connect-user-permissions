b143a9c4ddda414dbe924b46a5ad42329691f07c
almas-liaqat <Almas.liaqat@accenture.com>
2023-04-21 13:28:18 +0100

    initial commit

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -3,6 +3,7 @@ See
 https://github.com/NHSDigital/pytest-nhsd-apim/blob/main/tests/test_examples.py
 for more ideas on how to test the authorization of your API.
 """
+import logging
 import requests
 import pytest
 from os import getenv
@@ -13,25 +14,22 @@ def test_ping(nhsd_apim_proxy_url):
     resp = requests.get(f"{nhsd_apim_proxy_url}/_ping")
     assert resp.status_code == 200
 
-
 @pytest.mark.smoketest
 def test_wait_for_ping(nhsd_apim_proxy_url):
     retries = 0
     resp = requests.get(f"{nhsd_apim_proxy_url}/_ping")
     deployed_commitId = resp.json().get("commitId")
-
     while (deployed_commitId != getenv('SOURCE_COMMIT_ID')
-            and retries <= 30
-            and resp.status_code == 200):
+           and retries <= 30
+           and resp.status_code == 200):
         resp = requests.get(f"{nhsd_apim_proxy_url}/_ping")
         deployed_commitId = resp.json().get("commitId")
         retries += 1
-
+        logging.warning(f"Expected commit id {getenv('SOURCE_COMMIT_ID')} but was {deployed_commitId}, retrying")
     if resp.status_code != 200:
         pytest.fail(f"Status code {resp.status_code}, expecting 200")
     elif retries >= 30:
         pytest.fail("Timeout Error - max retries")
-
     assert deployed_commitId == getenv('SOURCE_COMMIT_ID')
 
 
@@ -49,44 +47,52 @@ def test_wait_for_status(nhsd_apim_proxy_url, status_endpoint_auth_headers):
     retries = 0
     resp = requests.get(f"{nhsd_apim_proxy_url}/_status", headers=status_endpoint_auth_headers)
     deployed_commitId = resp.json().get("commitId")
-
     while (deployed_commitId != getenv('SOURCE_COMMIT_ID')
-            and retries <= 30
-            and resp.status_code == 200
-            and resp.json().get("version")):
+           and retries <= 30
+           and resp.status_code == 200
+           and resp.json().get("version")):
         resp = requests.get(f"{nhsd_apim_proxy_url}/_status", headers=status_endpoint_auth_headers)
         deployed_commitId = resp.json().get("commitId")
         retries += 1
-
+        logging.warning(f"Expected commit id {getenv('SOURCE_COMMIT_ID')} but was {deployed_commitId}, retrying")
     if resp.status_code != 200:
         pytest.fail(f"Status code {resp.status_code}, expecting 200")
     elif retries >= 30:
         pytest.fail("Timeout Error - max retries")
     elif not resp.json().get("version"):
         pytest.fail("version not found")
-
     assert deployed_commitId == getenv('SOURCE_COMMIT_ID')
 
 
+@pytest.mark.auth
+@pytest.mark.integration
+@pytest.mark.user_restricted_separate_nhs_login
 @pytest.mark.nhsd_apim_authorization({"access": "application", "level": "level0"})
-def test_app_level0(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
-    resp = requests.get(f"{nhsd_apim_proxy_url}", headers=nhsd_apim_auth_headers)
-    assert resp.status_code == 401  # unauthorized
+def test_auth_level0(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
+    headers = {"Interaction-ID": "urn:nhs:names:services:gpconnect:fhir:operation:gpc.getstructuredrecord-1"}
+    headers.update(nhsd_apim_auth_headers)
+    resp = requests.get(f"{nhsd_apim_proxy_url}/Patient/9000000009", headers=headers)
+    assert resp.status_code == 401
 
 
-@pytest.mark.nhsd_apim_authorization({"access": "application", "level": "level3"})
-def test_app_level3(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
-    resp = requests.get(f"{nhsd_apim_proxy_url}", headers=nhsd_apim_auth_headers)
-    assert resp.status_code == 200
-
-
+@pytest.mark.auth
 @pytest.mark.nhsd_apim_authorization(
     {
-        "access": "healthcare_worker",
-        "level": "aal3",
-        "login_form": {"username": "656005750104"},
+        "access": "patient",
+        "level": "P9",
+        "login_form": {"username": "9912003071"}
     }
 )
-def test_cis2_aal3(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
-    resp = requests.get(f"{nhsd_apim_proxy_url}", headers=nhsd_apim_auth_headers)
+def test_nhs_login_p9(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
+    headers = {
+        "Interaction-ID": "urn:nhs:names:services:gpconnect:fhir:operation:gpc.getstructuredrecord-1",
+        "accept": "application/fhir+json",
+        "X-Correlation-ID": "11C46F5F-CDEF-4865-94B2-0EE0EDCC26DA",
+        "X-Request-ID": "60E0B220-8136-4CA5-AE46-1D97EF59D068"
+    }
+    headers.update(nhsd_apim_auth_headers)
+    resp = requests.get(
+        f"{nhsd_apim_proxy_url}/Patient/9000000009",
+        headers=headers
+    )
     assert resp.status_code == 200
