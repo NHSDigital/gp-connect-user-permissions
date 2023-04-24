90a8e0410ec98b5e7b8712591f937e6ec59285df
almas-liaqat <Almas.liaqat@accenture.com>
2023-04-24 15:55:07 +0100

    updated packages + removed speccy

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -74,26 +74,3 @@ def test_auth_level0(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
     headers.update(nhsd_apim_auth_headers)
     resp = requests.get(f"{nhsd_apim_proxy_url}/Patient/9000000009", headers=headers)
     assert resp.status_code == 401
-
-
-@pytest.mark.auth
-@pytest.mark.nhsd_apim_authorization(
-    {
-        "access": "patient",
-        "level": "P9",
-        "login_form": {"username": "9912003071"}
-    }
-)
-def test_nhs_login_p9(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
-    headers = {
-        "Interaction-ID": "urn:nhs:names:services:gpconnect:fhir:operation:gpc.getstructuredrecord-1",
-        "accept": "application/fhir+json",
-        "X-Correlation-ID": "11C46F5F-CDEF-4865-94B2-0EE0EDCC26DA",
-        "X-Request-ID": "60E0B220-8136-4CA5-AE46-1D97EF59D068"
-    }
-    headers.update(nhsd_apim_auth_headers)
-    resp = requests.get(
-        f"{nhsd_apim_proxy_url}/Patient/9000000009",
-        headers=headers
-    )
-    assert resp.status_code == 200
