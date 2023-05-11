8ad783ceeaa472eac16834c02adfa15d444b8f64
Jack McCaffrey <jack.mccaffrey1@nhs.net>
2023-05-11 15:15:28 +0100

    AMB-1511: Added test for external prism files

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -7,6 +7,7 @@ import logging
 import requests
 import pytest
 from os import getenv
+from .example_loader import load_example
 
 
 @pytest.mark.smoketest
@@ -99,3 +100,29 @@ def test_nhs_login_p9(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
         headers=headers
     )
     assert resp.status_code == 200
+
+
+@pytest.mark.auth
+@pytest.mark.integration
+@pytest.mark.nhsd_apim_authorization(
+    {
+        "access": "patient",
+        "level": "P9",
+        "login_form": {"username": "9912003071"}
+    }
+)
+def test_prism_returns_external_file(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
+    headers = {
+        "Interaction-ID": "urn:nhs:names:services:gpconnect:fhir:operation:gpc.getstructuredrecord-1",
+        "accept": "*/*",
+        "X-Correlation-ID": "11C46F5F-CDEF-4865-94B2-0EE0EDCC26DA",
+        "X-Request-ID": "60E0B220-8136-4CA5-AE46-1D97EF59D068"
+    }
+    headers.update(nhsd_apim_auth_headers)
+
+    resp = requests.get(
+        f"{nhsd_apim_proxy_url}/Patient/9000000009",
+        headers=headers
+    )
+    expected_response = load_example("GetResponsePermissionsFull.yaml")
+    assert resp.json() == expected_response.get("value")
