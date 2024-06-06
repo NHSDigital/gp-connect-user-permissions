548139bc575e15c401e3d9c505b568a530773fb1
amitkr-22 <amit.kumar22@nhs.net>
2024-06-06 16:20:06 +0100

    used try_parse

--- proxies/live/apiproxy/resources/jsc/GetGPCAuthCode.js
+++ proxies/live/apiproxy/resources/jsc/GetGPCAuthCode.js
@@ -1,5 +1,13 @@
-  var respContent=context.getVariable('GPCAuthResponse.content');
-  const respObject=JSON.parse(respContent);
-  
-  context.setVariable("request.header.Authorization", "Bearer " + respObject["access_token"]);
-  
+function json_tryparse(raw) {
+  try {
+      return JSON.parse(raw);
+  }
+  catch (e) {
+      return raw;
+  }
+}
+
+var respContent=context.getVariable('GPCPFSAuthResponse.content');
+const respObject=json_tryparse(respContent);
+
+context.setVariable("request.header.Authorization", "Bearer " + respObject["access_token"]);
