4be3b51433b2ef99aa5796e52245f99645a5551f
amitkr-22 <amit.kumar22@nhs.net>
2026-03-13 17:52:06 +0000

    fixed lint issues

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -8,7 +8,7 @@ import requests
 import pytest
 import os
 from os import getenv
-# from .example_loader import load_example
+from .example_loader import load_example
 
 
 @pytest.fixture()
@@ -118,16 +118,16 @@ def test_nhs_login_p9(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
    }
 )
 def test_prism_returns_external_file(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
-   headers = {
-       "accept": "*/*",
-       "X-Correlation-ID": "11C46F5F-CDEF-4865-94B2-0EE0EDCC26DA",
-       "X-Request-ID": "60E0B220-8136-4CA5-AE46-1D97EF59D068"
-   }
-   headers.update(nhsd_apim_auth_headers)
+    headers = {
+        "accept": "*/*",
+        "X-Correlation-ID": "11C46F5F-CDEF-4865-94B2-0EE0EDCC26DA",
+        "X-Request-ID": "60E0B220-8136-4CA5-AE46-1D97EF59D068"
+    }
+    headers.update(nhsd_apim_auth_headers)
 
-   resp = requests.get(
-       f"{nhsd_apim_proxy_url}/Patient/9000000009",
-       headers=headers
-   )
-   expected_response = load_example("GetResponsePermissionsFull.yaml")
-   assert resp.json() == expected_response.get("value")
+    resp = requests.get(
+        f"{nhsd_apim_proxy_url}/Patient/9000000009",
+        headers=headers
+    )
+    expected_response = load_example("GetResponsePermissionsFull.yaml")
+    assert resp.json() == expected_response.get("value")
