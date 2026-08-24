6226fcf29dff821fed69d178cf61b62f4f1e8ce0
Raghuram Grandhi <raghu.ramgrandhi1@nhs.net>
2026-08-24 15:21:17 +0100

    updates to sonar qube fixes.

--- proxies/live/apiproxy/resources/jsc/ValidateNHSNumber.js
+++ proxies/live/apiproxy/resources/jsc/ValidateNHSNumber.js
@@ -20,21 +20,18 @@ if (httpverb == 'GET') {
         }    
     }
 }
-else {
-    if(httpverb == 'POST') {
-        var reqContent = context.getVariable("request.content");
-        var jsonContent = JSON.parse(reqContent);
-        var postNHSnumber = null;
-        for (var i = 0; i < jsonContent.parameter.length; i++) {
-            var p = jsonContent.parameter[i];
-            if ( p.name === "patientNHSNumber" && p.valueIdentifier && p.valueIdentifier.value) {
-                postNHSnumber = p.valueIdentifier.value;
-                break;
-            }
+else if (httpverb == 'POST') {
+    var reqContent = context.getVariable("request.content");
+    var jsonContent = JSON.parse(reqContent);
+    var postNHSnumber = null;
+    for (var p of jsonContent.parameter) {
+        if (p.name === "patientNHSNumber" && p.valueIdentifier?.value) {
+            postNHSnumber = p.valueIdentifier.value;
+            break;
         }
-        print("postNHSnumber :" +postNHSnumber);
-        if (postNHSnumber && postNHSnumber !== actorNHS){
-            print("NHS Number is not valid");
-        }
+    }
+    print("postNHSnumber :" +postNHSnumber);
+    if (postNHSnumber && postNHSnumber !== actorNHS){
+        print("NHS Number is not valid");
     }
 }
